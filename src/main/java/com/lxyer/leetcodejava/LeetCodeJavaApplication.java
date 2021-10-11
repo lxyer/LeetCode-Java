f863f854591f5c6fda11dacf223becce5b06064f
lixiangyang <lixiangyang@qianxin.com>
2021-10-11 14:41:50 +0800

    Initial commit

---
+++ src/main/java/com/lxyer/leetcodejava/LeetCodeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lxyer.leetcodejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCodeJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lxyer/leetcodejava/LeetCodeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxyer.leetcodejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
